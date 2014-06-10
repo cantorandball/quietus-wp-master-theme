588e2e500ebb40e32e56e4490945ded1f964236b
Matt Johnson <matt@mattjohnson.io>
2014-06-10 17:30:27 +0100

    Add author template and fix link to it from post.

---
+++ author.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage The_Quietus_Master
+ * @since The Quietus Master 1.0
+ */
+
+get_header(); ?>
+
+<section>
+	<?php if ( have_posts() ): the_post(); ?>
+	<header>
+		<h1><?php the_author(); ?></h1>
+	</header>
+	<?php endif; ?>
+
+	<ul>
+		<?php
+		rewind_posts();
+		while( have_posts() ): the_post();
+			get_template_part( 'listing' );
+		endwhile;
+		?>
+	</ul>
+</section>
+
+<?php
+get_sidebar();
+get_footer(); ?>
