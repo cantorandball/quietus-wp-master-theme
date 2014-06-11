ab14b2ed67acb1c165d09d33efddc6e75336301a
Matt Johnson <matt@mattjohnson.io>
2014-06-11 14:12:01 +0100

    Template helper function to return posts sharing tags with the post provided.

--- inc/template-helpers.php
+++ inc/template-helpers.php
@@ -16,7 +16,35 @@ function get_latest_news_and_reviews() {
 		'category_name'  => 'news,album-reviews',
 		'posts_per_page' => 20,
 		'orderby'        => 'date'
+		);
+	return new WP_Query( $query_args );
+}
+
+/**
+ * Gets posts related to the one specified.
+ * Posts are related if they share any of the same tags
+ * @param  int $post_id ID of the post to find related posts for
+ * @return wp_query     wp_query object containing matching posts
+ */
+function get_related_posts( $post_id ) {
+	$post_tags = get_the_tags( $post_id );
+
+	if ( !$post_tags ) {
+		return new WP_Query();
+	}
+
+	$tags = array();
+	foreach ( $post_tags as $tag ) {
+		$tags[] = $tag->slug;
+	}
+
+	$query_args = array(
+		'tag'            => join( ',', $tags ),
+		'post__not_in'	 => array( $post_id ),
+		'posts_per_page' => 20,
+		'orderby'        => 'date'
 	);
+
 	return new WP_Query( $query_args );
 }
 ?>
