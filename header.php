680277aeecff66e519afcf157caa9fd2203c7d2d
Matt Johnson <matt@mattjohnson.io>
2014-06-11 15:36:49 +0100

    Link header back to home page.

--- header.php
+++ header.php
@@ -1,26 +1,31 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-	<head>
-		<meta charset="<?php bloginfo('charset');?>">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title><?php wp_title('|', true, 'right'); ?></title>
-		<?php wp_head(); ?>
-	</head>
 
-	<body>
-		<header id="header">
-			<h1 id="header__logo"><?php bloginfo('name'); ?></h1>
-			<div class="header__search">
-				<?php get_search_form(); ?>
-			</div>
-		</header>
-		<nav class="nav--global">
-			<ul class="nav--global__primary">
-				<?php wp_nav_menu( array(
-					'container' => false,
-					'items_wrap' => '%3$s',
-					'depth' => 0
-				) ); ?>
-		   </ul>
-		</nav>
+<head>
+	<meta charset="<?php bloginfo('charset');?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title><?php wp_title('|', true, 'right'); ?></title>
+	<?php wp_head(); ?>
+</head>
+
+<body>
+	<header id="header">
+		<h1 id="header__logo">
+			<a href="/" title="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>">
+				<?php bloginfo('name'); ?>
+			</a>
+		</h1>
+		<div class="header__search">
+			<?php get_search_form(); ?>
+		</div>
+	</header>
+	<nav class="nav--global">
+		<ul class="nav--global__primary">
+			<?php wp_nav_menu( array(
+				'container' => false,
+				'items_wrap' => '%3$s',
+				'depth' => 0
+			) ); ?>
+		</ul>
+	</nav>
