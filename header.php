2b16535badabe285d9d40d0f4ba9e2d3f17cdd3f
Alex Sanders <a@snd.rs>
2014-06-12 19:10:09 +0100

    add leaderboard add

--- header.php
+++ header.php
@@ -10,6 +10,41 @@
 </head>
 
 <body>
+	<div id="leaderboard">
+	  <script type="text/javascript">
+	    var googletag = googletag || {};
+	    googletag.cmd = googletag.cmd || [];
+	    (function() {
+	      var gads = document.createElement("script");
+	      gads.async = true;
+	      gads.type = "text/javascript";
+	      var useSSL = "https:" == document.location.protocol;
+	      gads.src = (useSSL ? "https:" : "http:") + "//www.googletagservices.com/tag/js/gpt.js";
+	      var node =document.getElementsByTagName("script")[0];
+	      node.parentNode.insertBefore(gads, node);
+	    })();
+	  </script>
+	  <div id="div-gpt-ad-988954517153641511-1">
+	    <script type='text/javascript'>
+	      googletag.cmd.push(function() {
+	        googletag.defineSlot('/16916245/thequietus.com', [[728, 90],[970, 250]],'div-gpt-ad-988954517153641511-1')
+	          .addService(googletag.pubads())
+	          .setTargeting("topic", "music");
+	        googletag.enableServices();
+	        googletag.display('div-gpt-ad-988954517153641511-1');
+	    });
+	    </script>
+	  </div>
+
+	  <script type="text/javascript">
+	    var height = $("leaderboard").getHeight();
+	    if(height >= 250)  $("leaderboard").setStyle({ position:  "static" });
+	    Event.observe(window, 'load', function() {
+	      var height = $("leaderboard").getHeight();
+	      if(height >= 250)  $("leaderboard").setStyle({ position:  "static" });
+	    });
+	  </script>
+	</div>
 	<header id="header">
 		<h1 id="header__logo">
 			<a href="/" title="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>">
