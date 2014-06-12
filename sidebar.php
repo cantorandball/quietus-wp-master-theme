a42516aef77f5eda3a420113ba3711cf71c77461
Alex Sanders <a@snd.rs>
2014-06-12 12:50:50 +0100

    add placeholder skyscraper

--- sidebar.php
+++ sidebar.php
@@ -4,6 +4,11 @@
  * Empty for now, but likely to include ad slots.
  */
 ?>
+
 <aside role="complementary" class="side-bar">
 	<?php get_template_part( 'latest-news-and-reviews' ); ?>
+	<!-- PLACEHOLDER ADVERT -->
+	<div id="skyscraper">
+	  <img src="http://pagead2.googlesyndication.com/simgad/813192542566921901" border="0" width="300" height="600" alt="" class="img_ad">
+	</div>
 </aside>
