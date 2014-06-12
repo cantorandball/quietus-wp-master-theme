6bee6357e4355a80394a225a40c6665ce529aa5d
Matt Johnson <matt@mattjohnson.io>
2014-06-12 10:42:51 +0100

    Remove red background and lorem ipsum.

--- sidebar-article.php
+++ sidebar-article.php
@@ -1,10 +1,4 @@
 <aside role="complementary" class="post__content-puffs">
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 	<?php
 	get_template_part( 'related-posts' );
 	get_template_part( 'latest-news-and-reviews' );
