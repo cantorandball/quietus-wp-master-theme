a5e72350061f66b6eefeb6253b132c7ba5ebdf32
Alex Sanders <a@snd.rs>
2014-06-13 17:38:15 +0100

    responsive puffs

--- listing.php
+++ listing.php
@@ -1,5 +1,5 @@
-<li class="puff" style="background-image:url('<?= wp_get_attachment_image_src( get_post_thumbnail_id($related_posts), 'medium' )[0] ?>')">
-	<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+<li class="puff">
+	<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" style="background-image:url('<?= wp_get_attachment_image_src( get_post_thumbnail_id($related_posts), 'medium' )[0] ?>')">
 		<span class="puff__title"><?php the_title(); ?></span>
 	</a>
 </li>
