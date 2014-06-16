8e2656863bb629fb80e68934a4d939387479d0b0
Alex Sanders <a@snd.rs>
2014-06-16 18:23:02 +0100

    insert ads into the article

--- _src/scripts/main.js
+++ _src/scripts/main.js
@@ -6,6 +6,13 @@ $(function(){
 	$('[data-advert]').each(function(i, advert) {
 		$('#' + advert.getAttribute('data-id')).html(advert.innerHTML);
 	});
+	// distribute article ads evenly between article paras
+	var $articleAds = $('[data-advert-article]');
+	var $articleParas = $('.post__content p');
+	var parasBeforeAd = Math.floor($articleParas.length / ($articleAds.length + 1));
+	$articleAds.each(function(i, ad){
+		$($articleParas[(i * parasBeforeAd) + parasBeforeAd]).before(ad.innerHTML);
+	})
 });
 
 // FTLabs fastclick library - removes any native delay on touch interfaces
--- footer.php
+++ footer.php
@@ -8,7 +8,6 @@
 		js = d.createElement(s); js.id = id;
 		js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1";
 		fjs.parentNode.insertBefore(js, fjs);
-		}(document, 'script', 'facebook-jssdk'));</script>
 		}(document, 'script', 'facebook-jssdk'));
 		</script>
 
@@ -21,6 +20,12 @@
 		</script>
 		<script type="text/template" data-advert data-id="skyscraper">
 			<img src="<?php echo get_template_directory_uri() . '/images/placeholders/ad-filmstrip.gif'; ?>" border="0" width="300" height="600" alt="" class="img_ad">
+		</script>
+		<script type="text/template" data-advert-article>
+			<img src="<?php echo get_template_directory_uri() . '/images/placeholders/ad-article-1.gif'; ?>" border="0" width="300" height="250" alt="" class="img_ad">
+		</script>
+		<script type="text/template" data-advert-article>
+			<img src="<?php echo get_template_directory_uri() . '/images/placeholders/ad-article-1.gif'; ?>" border="0" width="300" height="250" alt="" class="img_ad">
 		</script>
 	</body>
 </html>
