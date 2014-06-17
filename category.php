58ee95cdc2b7f0134cec36d840c795c4a8f69bcd
Matt Johnson <matt@mattjohnson.io>
2014-06-17 11:31:06 +0100

    Begin styling category page.
    Extracting shared css into mixins. Not sure if this is the appropriate
    approach?

--- category.php
+++ category.php
@@ -13,40 +13,45 @@ if ( $category->category_parent != '0' )
 get_header(); ?>
 
 <section role="main" class="category">
-	<header>
-		<?php if ( $parent ): ?>
-			<h3>
-				<a href="<?php echo get_category_link( $parent->cat_ID ); ?>"><?php echo $parent->name; ?></a>
-			</h3>
+	<div class="layout__jumbotron">
+		<header>
+			<?php if ( $parent ): ?>
+				<h3 class="category__parent-title">
+					<a href="<?php echo get_category_link( $parent->cat_ID ); ?>"><?php echo $parent->name; ?></a>
+				</h3>
+			<?php endif; ?>
+			<h2 class="category__title">
+				<?php single_cat_title( '', true ); ?>
+			</h2>
+		</header>
+
+		<?php if ( category_description() ): ?>
+			<section class="category__standfirst">
+				<?php echo category_description(); ?>
+			</section>
+		<?php endif ?>
+	</div>
+
+	<div class="layout__content">
+		<?php if ( have_posts() ): ?>
+			<div class="category__post-listing">
+				<ul>
+					<?php
+					while( have_posts() ): the_post();
+					get_template_part( 'listing', 'excerpt' );
+					endwhile;
+					?>
+				</ul>
+			</div>
+		<?php else: ?>
+			<div class="category__no-posts">
+				<p>There are no articles in this category.</p>
+			</div>
 		<?php endif; ?>
-		<h2>
-			<?php single_cat_title( '', true ); ?>
-		</h2>
-	</header>
-
-	<?php if ( category_description() ): ?>
-		<section class="category__standfirst">
-			<?php echo category_description(); ?>
-		</section>
-	<?php endif ?>
-
-	<?php if ( have_posts() ): ?>
-		<div class="category__post-listing">
-			<ul>
-				<?php
-				while( have_posts() ): the_post();
-				get_template_part( 'listing', 'excerpt' );
-				endwhile;
-				?>
-			</ul>
-		</div>
-	<?php else: ?>
-		<div class="category__no-posts">
-			<p>There are no articles in this category.</p>
-		</div>
-	<?php endif; ?>
-
-	<?php get_sidebar(); ?>
+	</div>
 </section>
 
-<?php get_footer(); ?>
+<?php
+get_sidebar();
+get_footer();
+?>
