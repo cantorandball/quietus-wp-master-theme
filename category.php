f20a352609d4e022842fe7f0211eb9186775f18e
Matt Johnson <matt@mattjohnson.io>
2014-06-17 10:56:22 +0100

    Basic category markup.

--- category.php
+++ category.php
@@ -5,24 +5,48 @@
  * @since The Quietus Master 1.0
  */
 
+$parent   = null;
+$category = get_queried_object();
+if ( $category->category_parent != '0' )
+	$parent = get_category( $category->category_parent );
+
 get_header(); ?>
 
-<section>
-	<?php if ( have_posts() ): ?>
+<section role="main" class="category">
 	<header>
-		<h1><?php single_cat_title( '', true ); ?></h1>
+		<?php if ( $parent ): ?>
+			<h3>
+				<a href="<?php echo get_category_link( $parent->cat_ID ); ?>"><?php echo $parent->name; ?></a>
+			</h3>
+		<?php endif; ?>
+		<h2>
+			<?php single_cat_title( '', true ); ?>
+		</h2>
 	</header>
+
+	<?php if ( category_description() ): ?>
+		<section class="category__standfirst">
+			<?php echo category_description(); ?>
+		</section>
+	<?php endif ?>
+
+	<?php if ( have_posts() ): ?>
+		<div class="category__post-listing">
+			<ul>
+				<?php
+				while( have_posts() ): the_post();
+				get_template_part( 'listing', 'excerpt' );
+				endwhile;
+				?>
+			</ul>
+		</div>
+	<?php else: ?>
+		<div class="category__no-posts">
+			<p>There are no articles in this category.</p>
+		</div>
 	<?php endif; ?>
 
-	<ul>
-		<?php
-		while( have_posts() ): the_post();
-			get_template_part( 'listing', 'excerpt' );
-		endwhile;
-		?>
-	</ul>
+	<?php get_sidebar(); ?>
 </section>
 
-<?php
-get_sidebar();
-get_footer(); ?>
+<?php get_footer(); ?>
