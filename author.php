3cb80e3c4815d95ac4835d8f0bea04a2ace7965f
Matt Johnson <matt@mattjohnson.io>
2014-06-18 14:33:25 +0100

    Author page style. Closes #25.

--- author.php
+++ author.php
@@ -7,21 +7,36 @@
 
 get_header(); ?>
 
-<section>
-	<?php if ( have_posts() ): the_post(); ?>
-	<header>
-		<h1><?php the_author(); ?></h1>
-	</header>
-	<?php endif; ?>
+<section role="main" class="author">
+	<div class="layout__jumbletron">
+		<header>
+			<h3 class="author__leadin">Articles from</h3>
+			<h2 class="author__title">
+				<?php if ( have_posts() ): the_post(); ?>
+					<?php
+					the_author();
+					rewind_posts();
+					?>
+				<?php endif; ?>
+			</h2>
+		</header>
+	</div>
 
-	<ul>
-		<?php
-		rewind_posts();
-		while( have_posts() ): the_post();
-			get_template_part( 'listing', 'excerpt' );
-		endwhile;
-		?>
-	</ul>
+	<div class="layout__content">
+		<?php if ( have_posts() ): ?>
+			<ul class="author__post-listing">
+				<?php
+				while( have_posts() ): the_post();
+				get_template_part( 'listing', 'excerpt' );
+				endwhile;
+				?>
+			</ul>
+		<?php else: ?>
+			<div class="author__no-posts">
+				<p>No articles form this author yet.</p>
+			</div>
+		<?php endif; ?>
+	</div>
 </section>
 
 <?php
