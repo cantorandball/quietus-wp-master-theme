9fa4c9a2f5c70b6a43625889c3b537cd7f558bb6
Matt Johnson <matt@mattjohnson.io>
2014-06-23 13:35:57 +0100

    Filter to determine template to use on category pages.

--- inc/wordpress-hooks.php
+++ inc/wordpress-hooks.php
@@ -55,6 +55,24 @@ function quietus_category_template( $single_template ) {
 
 add_filter( "single_template", "quietus_category_template" ) ;
 
+function quietus_category_template( $category_template ) {
+	global $child_categories;
+
+	$category = get_queried_object();
+	$child_categories = get_categories( array(
+		                           'type'         => 'post',
+		                           'parent'       => $category->cat_ID,
+		                           'hide_empty'   => 0,
+		                           'hierarchical' => 0
+		                           ) );
+
+	if ( $child_categories)
+		return get_template_directory() . '/category-root.php';
+
+	return $category_template;
+}
+add_filter( 'category_template', 'quietus_category_template' );
+
 /**
  * Forces the excerpt to conclude with a full stop, regardless of what's in the post.
  * @return string HTML-formatted post excerpt
