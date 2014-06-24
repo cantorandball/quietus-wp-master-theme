0eb31a4391ddb1cd7aaef386ddffac53039e7f2c
Matt Johnson <matt@mattjohnson.io>
2014-06-24 12:34:57 +0100

    Fix puffs.

--- template-parts/puff.php
+++ template-parts/puff.php
@@ -5,7 +5,7 @@ if ( $categories )
     $current_category = $categories[0]->slug;
 ?>
 <li class="puff <?= $current_category ?>">
-	<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" style="<?php the_permalink(); ?>" style="<?php echo get_background_image_url( get_the_ID(), 'medium' ); ?>">
+	<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" style="<?php echo get_background_image_url( get_the_ID(), 'medium' ); ?>">
 		<span class="puff__content"><?php the_title(); ?></span>
 	</a>
 </li>
