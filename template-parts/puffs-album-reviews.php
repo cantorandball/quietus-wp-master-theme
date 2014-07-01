75bcfc7569f6e81d2e4089af7d36e9bb334e9947
Alex Sanders <a@snd.rs>
2014-07-01 15:46:25 +0100

    format reviews

--- template-parts/puffs-album-reviews.php
+++ template-parts/puffs-album-reviews.php
@@ -4,10 +4,8 @@
  */
 $latest_reviews = get_latest_reviews();
 ?>
-<dl class="puffs puffs-album-reviews">
+<div class="puffs puffs-album-reviews">
 	<?php if ( $latest_reviews->have_posts() ): ?>
-	<dt class="puff-header"><?php echo( __( 'Latest Reviews', 'quietus' ) ); ?></dt>
-	<dd>
 		<ul class="puff-list">
 			<?php
 			while ( $latest_reviews->have_posts() ): $latest_reviews->the_post();
@@ -15,6 +13,5 @@ $latest_reviews = get_latest_reviews();
 			endwhile;
 			?>
 		</ul>
-	</dd>
 	<?php endif; ?>
-</dl>
+</div>
