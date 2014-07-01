735413a46f4d0085f897a74e7d5e18cd2bfd4cff
Alex Sanders <a@snd.rs>
2014-07-01 17:14:26 +0100

    hook in to the db

--- template-parts/digest.php
+++ template-parts/digest.php
@@ -8,31 +8,21 @@
 		<dt class="digest-section-title">Editors Picks</dt>
 		<dd class="digest-section">
 			<ol>
-				<li><a href=".">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem, blanditiis!</a></li>
-				<li><a href=".">Nisi consectetur, velit quaerat consequatur similique esse quasi beatae deserunt.</a></li>
-				<li><a href=".">Ex quod nolla perspiciatis. Libero, dolore! Vel accusamus, itaque enim!</a></li>
-				<li><a href=".">Omnis molestiae voluptas assumenda, sint dolorem quod commodi, exercitationem ipsum.</a></li>
-				<li><a href=".">Voluptas laboriosam id in nobis. Soluta cum necessitatibus sint odit!</a></li>
-				<li><a href=".">Repudiandae praesentium error, suscipit officia fuga in doloremque cupiditate placeat!</a></li>
-				<li><a href=".">Hic consequatur fugiat voluptates magnam, voluptate ollam sit rerum eos.</a></li>
-				<li><a href=".">Eveniet iusto ut atque totam blanditiis illum ea ad quidem.</a></li>
-				<li><a href=".">At veritatis architecto vel alias earum hic ipsum voluptas nolla.</a></li>
-				<li><a href=".">Voluptate enim sit doloribus ad tempore. Beatae sint, facere consequatur?</a></li>
+			<?php
+			$recent_posts = wp_get_recent_posts(array( 'category' => '4' ));
+			foreach( $recent_posts as $recent ){
+				echo '<li><a href="' . get_permalink($recent["ID"]) . '" title="' . esc_attr($recent["post_excerpt"]) . '" >' . $recent["post_title"] . '</a></li>';
+			} ?>
 			</ol>
 		</dd>
 		<dt class="digest-section-title">Most Recent</dt>
 		<dd class="digest-section">
 			<ol>
-				<li><a href=".">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, vel!</a></li>
-				<li><a href=".">In, eveniet! Corporis itaque nobis, labore, ducimus inventore odio quibusdam?</a></li>
-				<li><a href=".">Illo, inventore, similique! Voluptatem fuga maiores nesciunt quae provident temporibus!</a></li>
-				<li><a href=".">Odit corrupti neque optio inventore, unde molestiae quod quos tempore.</a></li>
-				<li><a href=".">Nolla est, aliquam deleniti cumque ipsa accusantium adipisci nihil explicabo.</a></li>
-				<li><a href=".">Voluptas aperiam, libero facilis animi repudiandae cupiditate error accusantium sequi.</a></li>
-				<li><a href=".">Aliquid blanditiis id esse asperiores similique, dolorem animi alias eum.</a></li>
-				<li><a href=".">Facilis commodi provident perspiciatis molestias distinctio similique ipsam quasi enim!</a></li>
-				<li><a href=".">Id voluptate laudantium omnis molestias, est ipsum veritatis colpa odit.</a></li>
-				<li><a href=".">Distinctio, laudantium minus voluptas alias. Perspiciatis reprehenderit et nemo esse.</a></li>
+			<?php
+			$recent_posts = wp_get_recent_posts();
+			foreach( $recent_posts as $recent ){
+				echo '<li><a href="' . get_permalink($recent["ID"]) . '" title="' . esc_attr($recent["post_excerpt"]) . '" >' . $recent["post_title"] . '</a></li>';
+			} ?>
 			</ol>
 		</dd>
 	</dl>
