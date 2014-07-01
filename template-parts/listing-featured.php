d37ee60da0248c238db99b31dced9c4690ad9371
Alex Sanders <a@snd.rs>
2014-07-01 13:59:25 +0100

    clean up tags/classes a bit more again

--- template-parts/listing-featured.php
+++ template-parts/listing-featured.php
@@ -9,8 +9,8 @@ if ( is_category() && get_queried_object_id() == get_the_category()[0]->cat_ID )
 	<div class="puff">
 		<a href="<?php the_permalink(); ?>" style="<?php echo get_background_image_url( get_the_ID(), 'medium' ); ?>">
 			<div class="puff-content">
-				<h3 class="category-title"><?php quietus_the_category( false ); ?></h3>
-				<h2 class="post-title"><?php the_title(); ?></h2>
+				<p class="category-title"><?php quietus_the_category( false ); ?></p>
+				<p class="post-title"><?php the_title(); ?></p>
 			</div>
 		</a>
 	</div>
--- template-parts/listing.php
+++ template-parts/listing.php
@@ -7,10 +7,8 @@ if ( is_category() && get_queried_object_id() == get_the_category()[0]->cat_ID )
 ?>
 <li class="listing">
 	<?php if ( $show_category ): ?>
-		<h3 class="category-title"><?php quietus_the_category( true ); ?></h3>
+		<p class="category-title"><?php quietus_the_category( true ); ?></p>
 	<?php endif; ?>
-	<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-		<h2 class="post-title"><?php the_title(); ?></h2>
-	</a>
+	<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" class="post-title"><?php the_title(); ?></a>
 	<div class="post-standfirst"><?php the_excerpt(); ?></div>
 </li>
