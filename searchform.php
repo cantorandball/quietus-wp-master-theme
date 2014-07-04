e60e529502ff53aec9dac35a20dbc446a92df178
Alex Sanders <a@snd.rs>
2014-07-04 17:14:27 +0100

    add placeholder text

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <form action="<?php echo home_url( '/' ); ?>" role="search" method="get" class="site-search">
-	<input type="search" name="s" id="s" placeholder="<?php echo( esc_attr( __( 'search', 'quietus' ) ) ); ?>" required value="<?php the_search_query(); ?>">
+	<input type="search" name="s" id="s" placeholder="<?php echo( esc_attr( __( 'e.g. grumbling fur, bakers dozen etc.', 'quietus' ) ) ); ?>" required value="<?php the_search_query(); ?>">
 	<button type="submit" class="<?php if(is_search()) {echo 'active';}; ?>"><?php echo( __( 'Search', 'quietus' ) ); ?>
 		<?php include(get_template_directory() . '/images/icon-search.svg'); ?>
 	</button>
