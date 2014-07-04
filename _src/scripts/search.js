c2231629ed5bb5fabc08ca398d7e0c5d80f48f25
Alex Sanders <a@snd.rs>
2014-07-04 17:13:45 +0100

    use request animation frame

--- _src/scripts/search.js
+++ _src/scripts/search.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var $ = require('zepto-browserify').$;
+var raf = require('raf');
 
 var $searchInput = $('.site-search input');
 var $searchForm = $('.site-search');
@@ -9,10 +10,14 @@ module.exports = {
 	init: function(){
 			$(document).on('click', 'form.site-search:not(.is-active) button[type=submit]', function(e) {
 			e.preventDefault();
-			// Move the cursor to the end of the current value
 			var value = $searchInput.val();
-			$searchInput.val('').val(value);
-			$searchForm.addClass('is-active').css({'position': 'relative'}).find('input').focus();
+			raf(function(){
+				$searchForm.addClass('is-active').css({'position': 'relative'});
+			})
+			raf(function(){
+				// Move the cursor to the end of the current value
+				$searchInput.val('').val(value).focus();
+			});
 		})
 
 		$(document).on('click', 'form.site-search button[type=button]', function(e) {
@@ -20,7 +25,7 @@ module.exports = {
 			// El needs position: relative to cover elements below while showing
 			// but :static to allow clicks through the <form> whilst hidden
 			// but can't be done with css, so we're doing it once the transition completes.
-			setTimeout(function(){$searchForm.css({'position': 'static'})}, 100)
+			setTimeout(raf(function(){$searchForm.css({'position': 'static'})}), 100)
 		})
 	}
 }
