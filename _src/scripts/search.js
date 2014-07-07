31b9ab05e549fb8288f6fefb7f4ad328ecf8c753
Alex Sanders <a@snd.rs>
2014-07-07 14:42:14 +0100

    fix nav being made unclickable by the safari antialiasing bug fix

--- _src/scripts/search.js
+++ _src/scripts/search.js
@@ -12,21 +12,12 @@ module.exports = {
 			$(document).on('click', '#site-search:not(.is-active) button[type=submit]', function(e) {
 				e.preventDefault();
 				searchForm.classList.add('is-active');
-				searchForm.style.position = 'relative';
 				searchInput.focus();
-				searchInput.select();
+				searchInput.value = searchInput.value;
 			})
 
 			$(document).on('click', '#site-search button[type=button]', function(e) {
 				searchForm.classList.remove('is-active');
-				// The form needs 'position: relative' to cover the nav elements below
-				// while showing, but 'position: static' to allow clicks to pass through
-				// the <form> whilst it's hidden (i.e. after the transition finishes).
-				// We can't set 'position: x' in a transition though, so we're doing it
-				// programatically once the transition completes.
-				setTimeout(raf(function(){
-					searchForm.style.position = 'static';
-				}), 100)
 			})
 		}
 	}
