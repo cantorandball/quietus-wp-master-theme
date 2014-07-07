c98bac6c00a379dde54daf3cea43551cf7cc9c48
Alex Sanders <a@snd.rs>
2014-07-07 12:48:56 +0100

    tidy up

--- _src/scripts/search.js
+++ _src/scripts/search.js
@@ -11,18 +11,22 @@ module.exports = {
 		if(!!searchInput && !!searchForm) {
 			$(document).on('click', '#site-search:not(.is-active) button[type=submit]', function(e) {
 				e.preventDefault();
-					searchInput.focus();
-					searchInput.select();
-					searchForm.classList.add('is-active');
-					searchForm.style.position = 'relative';
+				searchForm.classList.add('is-active');
+				searchForm.style.position = 'relative';
+				searchInput.focus();
+				searchInput.select();
 			})
 
 			$(document).on('click', '#site-search button[type=button]', function(e) {
 				searchForm.classList.remove('is-active');
-				// El needs position: relative to cover elements below while showing
-				// but :static to allow clicks through the <form> whilst hidden
-				// but can't be done with css, so we're doing it once the transition completes.
-				setTimeout(raf(function(){searchForm.style.position = 'static'}), 100)
+				// The form needs 'position: relative' to cover the nav elements below
+				// while showing, but 'position: static' to allow clicks to pass through
+				// the <form> whilst it's hidden (i.e. after the transition finishes).
+				// We can't set 'position: x' in a transition though, so we're doing it
+				// programatically once the transition completes.
+				setTimeout(raf(function(){
+					searchForm.style.position = 'static';
+				}), 100)
 			})
 		}
 	}
